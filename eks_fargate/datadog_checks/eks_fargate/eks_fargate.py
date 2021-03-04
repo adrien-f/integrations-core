ab58a34be540d9f836043145548e16e4b4616a59
Celene Chang <celene@datadoghq.com>
2021-03-04 08:42:33 -0500

    [eks_fargate] add capacity metrics

--- eks_fargate/datadog_checks/eks_fargate/eks_fargate.py
+++ eks_fargate/datadog_checks/eks_fargate/eks_fargate.py
@@ -1,31 +1,65 @@
 # (C) Datadog, Inc. 2020
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import logging
 import os
 
 from datadog_checks.base import AgentCheck
+from datadog_checks.base.utils.tagging import tagger
+
+log = logging.getLogger('collector')
+
+KUBELET_NODE_ENV_VAR = "DD_KUBERNETES_KUBELET_NODENAME"
+HOSTNAME_ENV_VAR = "HOSTNAME"
+CAPACITY_ENV_VAR = "DD_EKS_CAPACITY_ANNOTATION"
 
 
 class EksFargateCheck(AgentCheck):
     """
-    Generate a heartbeat for Amazon EKS on AWS Fargate workloads
+    Generate heartbeat and capacity metrics for Amazon EKS on AWS Fargate workloads
     """
 
     def __init__(self, name, init_config, instances):
         super(EksFargateCheck, self).__init__(name, init_config, instances)
-        pod_name = os.getenv("HOSTNAME")
-        virtual_node = os.getenv("DD_KUBERNETES_KUBELET_NODENAME", "")
+        self.NAMESPACE = "eks.fargate"
+        pod_name = os.getenv(HOSTNAME_ENV_VAR)
+        pod_id = os.getenv("DD_POD_UID", "")
+        virtual_node = os.getenv(KUBELET_NODE_ENV_VAR, "")
+        self.fargate_mode = pod_name is not None and "fargate" in virtual_node
 
-        self.fargate_mode = 'fargate' in virtual_node
+        self.tags = set()
 
-        if pod_name is not None and self.fargate_mode:
-            self.tags = []
-            self.tags.append("pod_name:" + pod_name)
-            self.tags.append("virtual_node:" + virtual_node)
-            self.tags.extend(instances[0].get('tags', []))
+        if self.fargate_mode:
+            self.tags.update(["pod_name:" + pod_name, "virtual_node:" + virtual_node])
+            self.tags.update(instances[0].get("tags", []))
+            self.tags.update(tagger.tag("kubernetes_pod_uid://%s" % pod_id, tagger.LOW) or [])
+            self.tags.update(os.getenv("DD_KUBERNETES_POD_LABELS_AS_TAGS", []))
 
     def check(self, instance):
-
-        # Only submit the heartbeat metric for fargate virtual nodes.
         if self.fargate_mode:
-            self.gauge("eks.fargate.pods.running", 1, self.tags)
+            # Submit the heartbeat metric for fargate virtual nodes.
+            self.gauge(self.NAMESPACE + ".pods.running", 1, self.tags)
+
+            # Submit metrics for resource capacities
+            capacity_annotation = os.getenv(CAPACITY_ENV_VAR, "")
+            if capacity_annotation != "":
+                cpu_val, mem_val = extract_resource_values(capacity_annotation)
+                if cpu_val == 0 and mem_val == 0:
+                    return
+                self.gauge(self.NAMESPACE + ".capacity.cpu", cpu_val, self.tags)
+                self.gauge(self.NAMESPACE + ".capacity.memory", mem_val, self.tags)
+
+
+def extract_resource_values(capacity_annotation):
+    # Given pod annotations, extract the resource values for submission as metrics
+    # "0.25vCPU 0.5GB"
+    cpu_val, mem_val = 0, 0
+    capacities = capacity_annotation.split(" ")
+    if len(capacities) != 2:
+        return cpu_val, mem_val
+    cpu, mem = capacities
+    if cpu.endswith("vCPU"):
+        cpu_val = float(cpu.strip("vCPU"))
+    if mem.endswith("GB"):
+        mem_val = float(mem.strip("GB"))
+    return cpu_val, mem_val
--- eks_fargate/tests/test_eks_fargate.py
+++ eks_fargate/tests/test_eks_fargate.py
@@ -3,21 +3,65 @@
 # Licensed under a 3-clause BSD style license (see LICENSE)
 from datadog_checks.dev import EnvVars
 from datadog_checks.eks_fargate import EksFargateCheck
+from datadog_checks.eks_fargate.eks_fargate import (
+    CAPACITY_ENV_VAR,
+    HOSTNAME_ENV_VAR,
+    KUBELET_NODE_ENV_VAR,
+    extract_resource_values,
+)
 
 
 def test_eksfargate(aggregator):
-    with EnvVars({'DD_KUBERNETES_KUBELET_NODENAME': 'fargate-foo', 'HOSTNAME': 'bar'}):
-        instance = {'tags': ['foo:bar']}
-        check = EksFargateCheck('eks_fargate', {}, [instance])
+    with EnvVars(
+        {
+            KUBELET_NODE_ENV_VAR: "fargate-foo",
+            HOSTNAME_ENV_VAR: "bar",
+            CAPACITY_ENV_VAR: "0.25vCPU 0.5GB",
+        }
+    ):
+        instance = {"tags": ["foo:bar"]}
+        check = EksFargateCheck("eks_fargate", {}, [instance])
         check.check(instance)
         aggregator.assert_metric(
-            "eks.fargate.pods.running", value=1, tags=["virtual_node:fargate-foo", "pod_name:bar", "foo:bar"]
+            check.NAMESPACE + ".pods.running", value=1, tags=["virtual_node:fargate-foo", "pod_name:bar", "foo:bar"]
+        )
+        aggregator.assert_metric(
+            check.NAMESPACE + ".capacity.cpu", value=0.25, tags=["virtual_node:fargate-foo", "pod_name:bar", "foo:bar"]
+        )
+        aggregator.assert_metric(
+            check.NAMESPACE + ".capacity.memory",
+            value=0.5,
+            tags=["virtual_node:fargate-foo", "pod_name:bar", "foo:bar"],
         )
 
 
 def test_not_eksfargate(aggregator):
-    with EnvVars({'DD_KUBERNETES_KUBELET_NODENAME': 'foo'}):
+    with EnvVars(
+        {
+            KUBELET_NODE_ENV_VAR: "foo",
+            CAPACITY_ENV_VAR: "0.25vCPU 0.5GB",
+        }
+    ):
         instance = {}
-        check = EksFargateCheck('eks_fargate', {}, [instance])
+        check = EksFargateCheck("eks_fargate", {}, [instance])
         check.check(instance)
-        assert "eks.fargate.pods.running" not in aggregator._metrics
+        assert check.NAMESPACE + ".pods.running" not in aggregator._metrics
+        assert check.NAMESPACE + ".capacity.cpu" not in aggregator._metrics
+        assert check.NAMESPACE + ".capacity.memory" not in aggregator._metrics
+
+
+def test_extract_resource_values():
+    test_input = "0.25vCPU 0.5GB"
+    cpu, mem = extract_resource_values(test_input)
+    assert cpu == 0.25
+    assert mem == 0.5
+
+    test_input = "0.25vCPUa 0.5GB"
+    cpu, mem = extract_resource_values(test_input)
+    assert cpu == 0
+    assert mem == 0.5
+
+    test_input = "0.25vCPU 0.5GBa"
+    cpu, mem = extract_resource_values(test_input)
+    assert cpu == 0.25
+    assert mem == 0
